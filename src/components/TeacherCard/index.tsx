8a04c47b85fc5f55677337be8c3d30925d14ffd0
Leo Salema <leo_viciogta@hotmail.com>
2024-04-07 07:45:19 -0300

    fix attributes names

--- src/components/TeacherCard/index.tsx
+++ src/components/TeacherCard/index.tsx
@@ -13,8 +13,8 @@ const TeacherCard = ({imageurl, name, profession,description}: Props) => {
   return(
     <S.CardContainer>
        <img src={imageurl} className='rounded' alt="" />
-          <Heading level={2} headingcolor='#000' headingalign='center'>{name}</Heading>
-          <Heading level={3} headingcolor='#000' headingalign='center'>{profession}</Heading>
+          <Heading level={2} color='#000' align='center'>{name}</Heading>
+          <Heading level={3} color='#000' align='center'>{profession}</Heading>
           <p>{description}</p>
     </S.CardContainer>
   )
